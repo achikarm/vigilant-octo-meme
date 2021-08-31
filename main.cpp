0c41a90ec3bc4b24b10ae962e49a0889c8f5b2c2
Akaash Chikarmane <akaash.chikarmane@intel.com>
2021-08-31 16:23:43 -0700

    new_branch2

--- main.cpp
+++ main.cpp
@@ -4,4 +4,5 @@ int main(void){
     std::cout << "Hello World!" << std::endl;
     std::cout << "Patch 1 rev 1" << std::endl;
     std::cout << "Patch 1 rev 2" << std::endl;
+    std::cout << "Patch 2 rev 1" << std::endl;
 }
